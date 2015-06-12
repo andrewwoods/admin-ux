2f587a9f87ad38c447f40e83304309a9ba44a75f
Andrew Woods <atwoods1@gmail.com>
2015-06-12 15:34:13 -0700

    Initial commit - generated by WP-CLI

---
+++ admin-ux.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Admin UX
+Version: 0.1-alpha
+Description: Add UX improvements to the WordPress Admin area.
+Author: awoods
+Author URI: http://andrewwoods.net
+Plugin URI: https://github.com/andrewwoods/admin-ux
+Text Domain: admin-ux
+Domain Path: /languages
+*/
+
